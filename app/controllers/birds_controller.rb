6c949d7ab381bf5047394a2a149cc4ae541676be
wandera7 <julius.wandera@student.moringaschool.com>
2022-09-24 21:27:08 +0300

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -42,6 +42,16 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+
+  end
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
   end
 
   private
